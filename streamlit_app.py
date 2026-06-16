13c23427c1abad8dba98c1701f05bba9a0916a30
Jagna Jezierska <161531007+jagnajezierska@users.noreply.github.com>
2026-06-16 13:25:35 +0200

    Add conversion to Pandas DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 import requests  
 
@@ -15,6 +16,10 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list=st.multiselect(
